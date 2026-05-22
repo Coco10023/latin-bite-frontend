966d85e4d3cd6c8c0b34101a6f9c23b143904eae
Coco10023 <mauricio2003@live.se>
2026-05-22 21:17:44 +0200

    Kopplar Angular frontend till REST API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Menu } from './pages/menu/menu';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Login } from './pages/login/login';
+import { Admin } from './pages/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'menu', component: Menu },
+  { path: 'about', component: About },
+  { path: 'login', component: Login },
+  { path: 'admin', component: Admin }
+];
--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuService } from '../../services/menu.services';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
-export class Menu {}
+export class Menu implements OnInit {
+
+    menuItems: any[] = [];
+
+    constructor(private menuService: MenuService) {}
+
+    ngOnInit(): void {
+      this.menuService.getMenu().subscribe((data) => {
+        this.menuItems = data;
+    });
+  }
+}
