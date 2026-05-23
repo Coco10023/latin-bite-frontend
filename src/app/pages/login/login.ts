b040b32e3ffacb0bb52583738f28fe25c5b8148d
Coco10023 <mauricio2003@live.se>
2026-05-23 18:13:05 +0200

    Kopplar auth.ts till login sidan

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(
+    private auth: Auth,
+    private router: Router
+  ) {}
+
+  login() {
+    const userData = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.auth.login(userData).subscribe({
+      next: (response: any) => {
+        this.auth.saveToken(response.token);
+        this.router.navigate(['/admin']);
+      },
+      error: () => {
+        this.errorMessage = 'Fel användarnamn eller lösenord';
+      }
+    });
+  }
+}
