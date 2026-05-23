ee3159b1b7aeb9a8058c0e4f68e6879879790487
Coco10023 <mauricio2003@live.se>
2026-05-23 23:46:21 +0200

    Lägger till en funktion på menu.services.ts

--- src/app/services/menu.services.ts
+++ src/app/services/menu.services.ts
@@ -11,7 +11,24 @@ export class MenuService {
 
   constructor(private http: HttpClient) {}
 
+  // Hämtar alla maträtter
   getMenu(): Observable<any> {
     return this.http.get(this.apiUrl);
+  }
+
+  // Skapar ny maträtt
+  addMenuItem(menuItem: any) {
+
+    // Hämtar JWT-token från sessionStorage
+    const token = sessionStorage.getItem('token');
+
+    // Skickar POST-request till backend
+    return this.http.post(this.apiUrl, menuItem, {
+
+      // Skickar med token i headers
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   }
 }
