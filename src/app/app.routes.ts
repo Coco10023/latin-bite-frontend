7ca2904ca401158cdbe66ead9687964f9de06369
Coco10023 <mauricio2003@live.se>
2026-05-24 18:39:48 +0200

    Lägger till/importerar guard på admin-route i app.routes.ts och ändrar på admin routen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Login } from './pages/login/login';
 import { Admin } from './pages/admin/admin';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'menu', component: Menu },
   { path: 'about', component: About },
   { path: 'login', component: Login },
-  { path: 'admin', component: Admin }
+  { path: 'admin', component: Admin,canActivate:[authGuard] }
 ];
